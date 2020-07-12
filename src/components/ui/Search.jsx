2d1f74976e7abbd6c1e1923a5cbf5926ed69b9f0
FemFX <ignedko34@gmail.com>
2020-07-12 09:58:00 +0300

    Create Search.jsx

---
+++ src/components/ui/Search.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState('');
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          className="form-control"
+          placeholder="Search"
+          autoFocus
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Search;
