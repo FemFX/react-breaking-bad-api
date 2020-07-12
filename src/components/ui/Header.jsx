ad8054857e7974ff320ecdc6cae22f9d609df269
FemFX <ignedko34@gmail.com>
2020-07-12 09:58:02 +0300

    Create Header.jsx

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const Header = () => (
+  <header className="center">
+    <img src={logo} alt="logo" />
+  </header>
+);
+export default Header;
